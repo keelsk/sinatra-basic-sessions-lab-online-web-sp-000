d01abd3f1bf4d56163d29e11edadabcaf5d18cda
Kirsten Keels <keelsk@gmail.com>
2020-03-29 02:12:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+class App < Sinatra:
+  configure do 
+    enable :sessions
+    set :session_secret, "Lefd3Z5yzrmioc2TkgiEPBcwR5L2DNOA"
+  end
+  
+  get '/' do 
+    erb :index
+  end
 end
